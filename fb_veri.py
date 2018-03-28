8416659bd2f6b73db085065bf550fa7a06641035
Sean Chickosky <chickosky@gmail.com>
2018-03-28 18:32:26 +0000

    Parameterized endpoint name

--- fb_veri.py
+++ fb_veri.py
@@ -1,6 +1,9 @@
 
 def handler(event, context):
-    # Dummy verification lambda, just replies 200 OK every time
+    # Dummy verification lambda, just replies 200 OK every time.
+    # Will need to pull the verification token and challenge string
+    #  out of event['queryStringParameters'] and verify the first, return
+    #  the second.
     response = {
         'statusCode': 200,
         'body': '',
