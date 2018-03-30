b34d4ee535edb4a49107e956da7c24aee2030fde
fdong081 <fenfangdong@gmail.com>
2018-03-30 16:54:16 -0400

    Update fb_veri.py

--- fb_veri.py
+++ fb_veri.py
@@ -1,14 +1,35 @@
+'use strict';
 
-def handler(event, context):
-    # Dummy verification lambda, just replies 200 OK every time.
-    # Will need to pull the verification token and challenge string
-    #  out of event['queryStringParameters'] and verify the first, return
-    #  the second.
-    response = {
-        'statusCode': 200,
-        'body': '',
-        'headers': {
-            'Content-Type': '*/*'
-        }
+#var USER_TOKEN = "EAACuOJLUNcoBAL8PoBGXgDVrKvv05oPsvclS0YDavfozZAl86WcHkYTM5kcalUQkt3ZBoPwjfcr07Rryf8bFfzqnjZB71erjR4gSK2tswNPOGbNmT7HrhCUe0PDvRQx864sMl9849jk8gN7C0pJff9Hx9QgYTIZD";
+var VERIFY_TOKEN = "Access_Token";
+
+def handler (event, context):
+ 
+  if(event['queryStringParameters']){
+    queryParams = event['queryStringParameters'];
+ 
+    rVerifyToken = queryParams['hub.verify_token']
+ 
+    if (rVerifyToken === VERIFY_TOKEN) {
+      challenge = queryParams['hub.challenge']
+      
+      response = {
+        "body": parseInt(challenge),
+        "statusCode": 200
+      }
+      return response
+    }else{
+       response = {
+        "body": 'Error, wrong validation token ' + rVerifyToken,
+        "statusCode": 422
+      }
+      return response
     }
+  }
+  else:
+    response = {
+        "body": "varifification Failed "
+     };
     return response
+  
+  
