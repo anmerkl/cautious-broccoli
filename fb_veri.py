f9e8d178e0d567816111c8c577ff099994c127da
Sean Chickosky <chickosky@gmail.com>
2018-04-06 20:07:07 +0000

    Pythonified verification lambda

--- fb_veri.py
+++ fb_veri.py
@@ -1,35 +1,26 @@
-'use strict';
-
-#var USER_TOKEN = "EAACuOJLUNcoBAL8PoBGXgDVrKvv05oPsvclS0YDavfozZAl86WcHkYTM5kcalUQkt3ZBoPwjfcr07Rryf8bFfzqnjZB71erjR4gSK2tswNPOGbNmT7HrhCUe0PDvRQx864sMl9849jk8gN7C0pJff9Hx9QgYTIZD";
-var VERIFY_TOKEN = "Access_Token";
+VERIFY_TOKEN = "Access_Token"
 
 def handler (event, context):
- 
-  if(event['queryStringParameters']){
-    queryParams = event['queryStringParameters'];
- 
-    rVerifyToken = queryParams['hub.verify_token']
- 
-    if (rVerifyToken === VERIFY_TOKEN) {
-      challenge = queryParams['hub.challenge']
-      
-      response = {
-        "body": parseInt(challenge),
-        "statusCode": 200
-      }
-      return response
-    }else{
-       response = {
-        "body": 'Error, wrong validation token ' + rVerifyToken,
-        "statusCode": 422
-      }
-      return response
-    }
-  }
-  else:
-    response = {
-        "body": "varifification Failed "
-     };
-    return response
-  
-  
+    #print(event.keys())
+    if event["queryStringParameters"]:
+        queryParams = event['queryStringParameters']
+        rVerifyToken =  queryParams['hub.verify_token']
+        if rVerifyToken == VERIFY_TOKEN:
+            challenge = queryParams['hub.challenge']
+            response = {
+              "body": int(challenge),
+              "statusCode": 200
+              }
+            return response
+        else:
+            response = {
+                "body": 'Error, wrong validation token ' + rVerifyToken,
+                "statusCode": 422
+                
+            }
+            return response
+    else:
+        response = {
+            "body": "varifification Failed "
+        }
+        return response
